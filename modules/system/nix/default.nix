803e07030692de0859a8ae57ea44f65749cfb3eb
Ohma Togaki <contact@younagi.dev>
2026-01-27 01:47:00 +0900

    style: format nix module

--- modules/system/nix/default.nix
+++ modules/system/nix/default.nix
@@ -11,7 +11,7 @@ let
     mkForce
     mkIf
     mkMerge
-mkPackageOption
+    mkPackageOption
     ;
   cfg = config.yakumo.system.nix;
   systemRole = config.yakumo.system.role;
@@ -34,50 +34,54 @@ in
         ];
       };
     })
-    (mkIf systemRole == "workstation" {
-      nix = {
-        gc = {
-          automatic = true;
-          dates = "weekly";
-          options = "--delete-older-than 7d";
-          # Ensure GC runs on boot if missed.
-          persistent = true;
+    (
+      mkIf systemRole == "workstation" {
+        nix = {
+          gc = {
+            automatic = true;
+            dates = "weekly";
+            options = "--delete-older-than 7d";
+            # Ensure GC runs on boot if missed.
+            persistent = true;
+          };
+          settings = {
+            trusted-users = [
+              "root"
+              "@wheel"
+            ];
+            auto-optimise-store = true;
+          };
         };
-        settings = {
-          trusted-users = [
-            "root"
-            "@wheel"
-          ];
-          auto-optimise-store = true;
+      }
+    )
+    (
+      mkIf systemRole == "server" {
+        nix = {
+          gc = {
+            # Results in mountains of garbage otherwise.
+            automatic = mkForce true;
+            dates = "weekly";
+            options = "--delete-older-than 14d";
+          };
+          settings = {
+            trusted-users = [
+              "root"
+            ];
+            # If set to true, this "Auto" optimisation runs on every file write.
+            # On a server performing updates or builds, this could add significant
+            # CPU/IO overhead.
+            auto-optimise-store = mkForce false;
+            # The maximum number of jobs Nix tries to build in parallel.
+            # max-jobs = "2"; # Default: "auto"
+          };
+          optimise = {
+            automatic = mkForce true;
+            dates = [
+              "03:30" # In the dead of night...
+            ];
+          };
         };
-      };
-    })
-    (mkIf systemRole == "server" {
-      nix = {
-        gc = {
-          # Results in mountains of garbage otherwise.
-          automatic = mkForce true;
-          dates = "weekly";
-          options = "--delete-older-than 14d";
-        };
-        settings = {
-          trusted-users = [
-            "root"
-          ];
-          # If set to true, this "Auto" optimisation runs on every file write.
-          # On a server performing updates or builds, this could add significant
-          # CPU/IO overhead.
-          auto-optimise-store = mkForce false;
-          # The maximum number of jobs Nix tries to build in parallel.
-          # max-jobs = "2"; # Default: "auto"
-        };
-        optimise = {
-          automatic = mkForce true;
-          dates = [
-            "03:30" # In the dead of night...
-          ];
-        };
-      };
-    })
+      }
+    )
   ];
 }
