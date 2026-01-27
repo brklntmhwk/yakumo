673236ed1c4e210edb72be3c740c1d1d6e6d8fe6
Ohma Togaki <contact@younagi.dev>
2026-01-27 12:42:06 +0900

    feat: add xdg module and make some changes accordingly

--- modules/ai/mcp/filesystem/default.nix
+++ modules/ai/mcp/filesystem/default.nix
@@ -7,25 +7,41 @@
 }:
 
 let
-  inherit (lib) mkEnableOption mkIf;
+  inherit (lib)
+    mkEnableOption
+    mkIf
+    mkOption
+    types
+    ;
   cfg = config.yakumo.ai.mcp.filesystem;
 in
 {
   options.yakumo.ai.mcp.filesystem = {
     enable = mkEnableOption "Filesystem MCP Server";
+    filePaths = mkOption {
+      type = types.listOf types.str;
+      default = [ ];
+      description = "List of file paths to become accessible.";
+    };
   };
 
   config = mkIf cfg.enable (
     let
       inherit (inputs) mcp-servers;
+      inherit (lib) optional;
+      userCfg = config.yakumo.user;
+      xdgCfg = config.yakumo.xdg;
     in
     {
       yakumo.ai.mcp.servers =
         (mcp-servers.lib.evalModule pkgs {
           programs.filesystem = {
             enable = true;
-            # TODO: add a directory path.
-            # args = [];
+            args =
+              optional (cfg.filePaths != [ ]) cfg.filePaths
+              ++ optional xdgCfg.enable [
+                "${userCfg.home}/Documents"
+              ];
           };
         }).config.settings.servers;
     }
---
+++ modules/xdg/default.nix
@@ -0,0 +1,66 @@
+{
+  config,
+  lib,
+  pkgs,
+  ...
+}:
+
+let
+  inherit (lib)
+    mkEnableOption
+    mkIf
+    ;
+  cfg = config.yakumo.xdg;
+  userCfg = config.yakumo.user;
+  xdgDirs = [
+    "Desktop"
+    "Documents"
+    "Downloads"
+    "Music"
+    "Pictures"
+    "Public"
+    "Templates"
+    "Videos"
+    ".config"
+    ".local/share"
+    ".local/state"
+    ".cache"
+  ];
+  # 'd': Create directory if it does not exist.
+  # Otherwise, it safely fixes permissions/ownership without touching content.
+  # Format: Type Path Mode User Group Age Argument
+  mkXdgRule = dir: "d /home/${userCfg.name}/${dir} 0755 ${userCfg.name} ${userCfg.group} - -";
+in
+{
+  options.yakumo.xdg = {
+    enable = mkEnableOption "XDG Base Directory";
+  };
+
+  config = mkIf cfg.enable {
+    assertions = [
+      {
+        assertion = userCfg ? name;
+        message = "yakumo.xdg requires yakumo.user.name to be set.";
+      }
+    ];
+
+    # e.g., `[ "d /home/otogaki/Documents 0755 otogaki yakumo - -" ... ]`
+    systemd.tmpfiles.rules = map mkXdgRule xdgDirs;
+
+    environment.sessionVariables = {
+      XDG_CACHE_HOME  = "$HOME/.cache";
+      XDG_CONFIG_HOME = "$HOME/.config";
+      XDG_DATA_HOME   = "$HOME/.local/share";
+      XDG_STATE_HOME  = "$HOME/.local/state";      
+      # Add user binaries to PATH just in case.
+      PATH = [ 
+        "$HOME/.local/bin"
+      ];
+    };
+
+    # This package provides `xdg-user-dirs-update`, which generates
+    # '~/.config/user-dirs.dirs'. Some apps consult this file to know where
+    # your "Music" or "Downloads" folders actually are.
+    environment.systemPackages = [ pkgs.xdg-user-dirs ];
+  };
+}
