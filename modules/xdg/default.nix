6d47e63b6b1a1472b3ade8df1979d15f1b6e3610
Ohma Togaki <contact@younagi.dev>
2026-01-28 01:44:36 +0900

    style: format xdg module file

--- modules/xdg/default.nix
+++ modules/xdg/default.nix
@@ -48,12 +48,12 @@ in
     systemd.tmpfiles.rules = map mkXdgRule xdgDirs;
 
     environment.sessionVariables = {
-      XDG_CACHE_HOME  = "$HOME/.cache";
+      XDG_CACHE_HOME = "$HOME/.cache";
       XDG_CONFIG_HOME = "$HOME/.config";
-      XDG_DATA_HOME   = "$HOME/.local/share";
-      XDG_STATE_HOME  = "$HOME/.local/state";      
+      XDG_DATA_HOME = "$HOME/.local/share";
+      XDG_STATE_HOME = "$HOME/.local/state";
       # Add user binaries to PATH just in case.
-      PATH = [ 
+      PATH = [
         "$HOME/.local/bin"
       ];
     };
