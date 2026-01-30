569e99eac88a9d6a98ff07fb91a7e824d5aa38e1
Ohma Togaki <contact@younagi.dev>
2026-01-30 13:25:04 +0900

    feat: add utsusemi host config and refactor some common settings

--- hosts/utsusemi/default.nix
+++ hosts/utsusemi/default.nix
@@ -2,7 +2,6 @@
   inputs,
   config,
   lib,
-  pkgs,
   ...
 }:
 
@@ -20,6 +19,19 @@
     nix = {
       enableFlake = true;
     };
+  };
+
+  i18n = {
+    defaultLocale = "en_US.UTF-8";
+    extraLocaleSettings = {
+      # Run `locale` to see available options
+    };
+    extraLocales = [
+      "en_US.UTF-8/UTF-8"
+      "en_GB.UTF-8/UTF-8"
+      "es_ES.UTF-8/UTF-8"
+      "ja_JP.UTF-8/UTF-8"
+    ];
   };
 
   # Make this align with the NixOS-WSL version.
---
+++ users/otogaki/hosts/utsusemi/default.nix
@@ -0,0 +1,75 @@
+{
+  config,
+  lib,
+  pkgs,
+  ...
+}:
+
+{
+  imports = [
+    ../common # Common configs among user's hosts
+  ];
+
+  yakumo.system = {
+    i18n = {
+      inputMethod = {
+        enable = true;
+        fcitx5 = {
+          enable = true;
+          extraAddons = [ ];
+          quickPhrase = { };
+        };
+      };
+    };
+  };
+
+  yakumo.desktop = {
+    terminal = {
+      wezterm = {
+        enable = true;
+        settings = import ../../configs/wezterm { inherit theme; };
+      };
+    };
+    apps = {
+      browsers = {
+        
+      };
+    };
+  };
+
+  yakumo.editors = {
+    emacs = {
+      enable = true;
+      ametsuchi.enable = true;
+    };
+  };
+
+  yakumo.programs = {
+    wthrr = {
+      enable = true;
+      settings = import ../../configs/wthrr { };
+    };
+  };
+
+  yakumo.services = {
+    xremap = {
+      enable = true;
+      userName = config.yakumo.user.name;
+      serviceMode = "user";
+      config = import ../../configs/xremap { };
+    };
+  };
+
+  fonts = {
+    # 'fonts.packages' are configured in the common file.
+    fontDir.enable = true;
+    fontconfig = {
+      defaultFonts = {
+        serif = [ fonts.notoCjkSerif.name ];
+        sansSerif = [ fonts.notoCjkSans.name ];
+        monospace = [ fonts.hackgenNf.name ];
+        emoji = [ fonts.notoEmoji.name ];
+      };
+    };
+  };
+}
