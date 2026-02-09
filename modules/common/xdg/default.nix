e32956b84ac7b365355f51d0498d136fb4e95633
Ohma Togaki <contact@younagi.dev>
2026-02-09 10:00:23 +0900

    fix: fix 'defined multiple times' error in zsh module

--- modules/common/xdg/default.nix
+++ modules/common/xdg/default.nix
@@ -35,10 +35,6 @@ in
         XDG_CONFIG_HOME = "$HOME/.config";
         XDG_DATA_HOME = "$HOME/.local/share";
         XDG_STATE_HOME = "$HOME/.local/state";
-        # Add user binaries to PATH just in case.
-        PATH = [
-          "$HOME/.local/bin"
-        ];
       };
 
       # This package provides `xdg-user-dirs-update`, which generates
