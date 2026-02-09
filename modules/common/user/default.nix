e8114b7a96755cc2995a8fb1c4e4a2e69ca757de
Ohma Togaki <contact@younagi.dev>
2026-02-09 16:33:43 +0900

    chore: tweak user module

--- modules/common/user/default.nix
+++ modules/common/user/default.nix
@@ -37,26 +37,30 @@ in
     # Prefer 'yakumo.user.*' over 'users.users.<username>.*'.
     users.users.${cfg.name} = mkAliasDefinitions options.yakumo.user;
 
-    # TEMPORARY: Enabled to allow manual password recovery.
-    # TODO: Revert to 'mkForce false' once Sops secrets are fixed.
-    users.mutableUsers = true;
-
-    # Set a placeholder password to satisfy the NixOS anti-lockout assertion.
-    users.users.root.initialPassword = "password";
+    users = {
+      mutableUsers = false;
+      users = {
+        # TODO: migrate to hashedPasswordFile. Add login_password_root to the
+        # root sops file.
+        root.initialPassword = "password";
+        # root.hashedPasswordFile = config.sops.secrets.login_password_root.path;
+      };
+    };
 
     yakumo.user = {
       description = mkDefault cfg.name;
-      extraGroups = mkDefault [ "wheel" ];
-      group = mkDefault "users";
+      extraGroups = [ "wheel" ];
+      group = "users";
       # If set to true, this sets:
       # - 'createHome' to true
       # - 'home' to '/home/<username>'
       # - 'useDefaultShell' to true
       # - 'isSystemUser' to false
-      isNormalUser = mkDefault true;
+      isNormalUser = true;
       # Explicitly define this so it can be read by other modules.
       home = if pkgs.stdenv.isDarwin then "/Users/${cfg.name}" else "/home/${cfg.name}";
       uid = mkDefault 1000;
+      # TODO: remove this after figuring out how to manage passwords.
       # Set a placeholder password to satisfy the NixOS anti-lockout assertion.
       # This enables you to use 'sudo'. You can change it with the `passwd` command later.
       initialPassword = "password";
