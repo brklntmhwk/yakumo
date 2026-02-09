02192acb157ca5a43cec5cc465c9e526f339b4c2
Ohma Togaki <contact@younagi.dev>
2026-02-09 15:34:30 +0900

    chore: modify binary cache settings and nix options

--- flake.nix
+++ flake.nix
@@ -7,12 +7,10 @@
 
   nixConfig = {
     extra-substituters = [
-      "https://nix-community.cachix.org"
       "https://hyprland.cachix.org"
       "https://brklntmhwk.cachix.org"
     ];
     extra-trusted-public-keys = [
-      "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
       "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="
       "brklntmhwk.cachix.org-1:mGWjznSV6FglvHR7/2sa4MrCtGHMLiAOc9Ru+tEkdyg="
     ];
