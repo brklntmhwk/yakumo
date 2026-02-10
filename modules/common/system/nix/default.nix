fc9eb88f7ced83a060e36e9d5886bbe640033d40
Ohma Togaki <contact@younagi.dev>
2026-02-10 13:20:59 +0900

    fix: put nix module options to correct namespaces

--- modules/common/system/nix/default.nix
+++ modules/common/system/nix/default.nix
@@ -14,18 +14,20 @@ in {
     {
       nix = {
         package = cfg.package;
-        # Allow these users to connect to the Nix daemon.
-        allowed-users = [ "@wheel" ];
-        # List of binary cache URLs to obtain pre-built binaries of Nix packages.
-        # `trusted-substituters` differs from this in that non-root users can
-        # also use it.
-        substituters =
-          [ "https://cache.nixos.org" "https://nix-community.cachix.org" ];
-        # List of public keys used to sign binary caches.
-        trusted-public-keys = [
-          "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
-          "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
-        ];
+        settings = {
+          # Allow these users to connect to the Nix daemon.
+          allowed-users = [ "@wheel" ];
+          # List of binary cache URLs to obtain pre-built binaries of Nix packages.
+          # `trusted-substituters` differs from this in that non-root users can
+          # also use it.
+          substituters =
+            [ "https://cache.nixos.org" "https://nix-community.cachix.org" ];
+          # List of public keys used to sign binary caches.
+          trusted-public-keys = [
+            "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
+            "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+          ];
+        };
       };
     }
     (mkIf cfg.enableFlake {
