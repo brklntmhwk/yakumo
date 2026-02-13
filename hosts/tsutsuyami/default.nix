d54a8833c761b1fdde635802a3326695e056a912
Ohma Togaki <contact@younagi.dev>
2026-02-13 12:08:03 +0900

    chore: prepare yosuga configs for all hosts

--- hosts/tsutsuyami/default.nix
+++ hosts/tsutsuyami/default.nix
@@ -16,6 +16,20 @@ in {
       manager = "networkmanager";
       wifi.enable = true;
     };
+    # persistence.yosuga = {
+    #   enable = true;
+    #   directories = [
+    #     "/etc/nixos"
+    #     "/etc/NetworkManager/system-connections"
+    #   ];
+    #   files = [
+    #     "/etc/machine-id"
+    #     "/etc/ssh/ssh_host_ed25519_key"
+    #     "/etc/ssh/ssh_host_ed25519_key.pub"
+    #     "/etc/ssh/ssh_host_rsa_key"
+    #     "/etc/ssh/ssh_host_rsa_key.pub"
+    #   ];
+    # };
   };
 
   yakumo.hardware = {
