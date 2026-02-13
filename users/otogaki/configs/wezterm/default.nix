9ee789e269eaa6506a45c972e0b2c7fe3f2bca2d
Ohma Togaki <contact@younagi.dev>
2026-02-13 11:38:59 +0900

    chore: modify wezterm config

--- users/otogaki/configs/wezterm/default.nix
+++ users/otogaki/configs/wezterm/default.nix
@@ -38,8 +38,8 @@ in {
   font_size = 16.0;
   default_cursor_style = "SteadyBar";
   use_ime = true;
-  window_background_opacity = 0.6;
-  macos_window_background_blur = 10;
+  # window_background_opacity = 0.85;
+  # macos_window_background_blur = 10;
   window_padding = {
     left = 15;
     right = 15;
