4aa2361ca037c3991077a866b193541fe3f831ba
Ohma Togaki <contact@younagi.dev>
2026-02-17 19:01:59 +0900

    feat: add brave browser module

---
+++ modules/common/desktop/apps/browsers/brave/default.nix
@@ -0,0 +1,32 @@
+{
+  config,
+  lib,
+  pkgs,
+  ...
+}:
+
+let
+  inherit (lib) mkEnableOption mkIf;
+  cfg = config.yakumo.desktop.apps.browsers.brave;
+in
+{
+  options.yakumo.desktop.apps.browsers.brave = {
+    enable = mkEnableOption "brave";
+  };
+
+  config = mkIf cfg.enable {
+    yakumo.user.packages =
+      builtins.attrValues {
+        inherit (pkgs) brave;
+      }
+      ++ (pkgs.makeDesktopItem {
+        name = "brave";
+        desktopName = "Brave";
+        description = "Brave Browser.";
+        genericName = "Web Browser";
+        icon = "brave";
+        exec = "${brave}/bin/brave --incognito";
+        categories = [ "Network" ];
+      });
+  };
+}
