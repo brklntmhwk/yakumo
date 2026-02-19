184dbc2d654657f6d8c16d3c950bda5628ddae95
Ohma Togaki <contact@younagi.dev>
2026-02-19 16:10:50 +0900

    feat: add wireplumber as pipewire backend

--- modules/nixos/hardware/audio/default.nix
+++ modules/nixos/hardware/audio/default.nix
@@ -24,6 +24,7 @@ in
       alsa.support32Bit = true;
       pulse.enable = true;
       jack.enable = true;
+      wireplumber.enable = true;
     };
     security.rtkit.enable = true;
     yakumo.user = {
