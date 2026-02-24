3c7bee37b8cbbffc82e14f55b3be8d8a1dcc468d
Ohma Togaki <contact@younagi.dev>
2026-02-24 11:41:19 +0900

    feat: prepare services (still WIP)

---
+++ modules/nixos/services/calibre/default.nix
@@ -0,0 +1,68 @@
+# WIP
+{
+  config,
+  lib,
+  ...
+}:
+
+let
+  inherit (lib)
+    mkEnableOption
+    mkIf
+    mkMerge
+    ;
+  cfg = config.yakumo.services.calibre;
+  systemCfg = config.yakumo.system;
+in
+{
+  options.yakumo.services.calibre = {
+    enable = mkEnableOption "calibre";
+  };
+
+  config = mkIf cfg.enable (mkMerge [
+    (mkIf (systemCfg.role == "workstation") {
+      services.calibre-web = {
+        enable = true;
+        group = "calibre-web"; # Default: 'calibre-web'
+        user = "calibre-web"; # Default: 'calibre-web'
+        # Note that this will be concatenated with '/var/lib/' if not specified
+        # as an absolute path (e.g., '/path/to/calibre-web-data').
+        dataDir = "calibre-web";
+        openFirewall = false; # Default: false
+        listen = {
+          ip = "::1"; # Default: '::1'
+          port = 8083; # Default: 8083
+        };
+        options = {
+          # Path to Calibre library.
+          calibreLibrary = null; # Default: null
+          enableBookConversion = false; # Default: false
+          enableBookUploading = false; # Default: false
+          enableKepubify = false; # Default: false
+          reverseProxyAuth = {
+            enable = true; # Default: false
+            header = ""; # Default: ''
+          };
+        };
+      };
+    })
+    (mkIf (systemCfg.role == "server") {
+      services.calibre-server = {
+        enable = true;
+        group = "calibre-server"; # Default: 'calibre-server'
+        user = "calibre-server"; # Default: 'calibre-server'
+        host = "0.0.0.0"; # Default: '0.0.0.0'
+        port = 8080; # Default: 8080
+        openFirewall = false; # Default: false
+        extraFlags = [];
+        auth = {
+          enable = true; # Default: false
+          mode = "auto"; # Default: 'auto' (Options: 'basic', 'digest')
+          # Choose users DB file to use for authentication.
+          # Ensure to initialize it before service startup.
+          userDb = null; # Default: null
+        };
+      };
+    })
+  ]);
+}
---
+++ modules/nixos/services/paperless-ngx/default.nix
@@ -0,0 +1,54 @@
+# WIP
+{
+  config,
+  lib,
+  ...
+}:
+
+let
+  inherit (lib)
+    mkEnableOption
+    mkIf
+    ;
+  cfg = config.yakumo.services.paperless-ngx;
+in
+{
+  options.yakumo.services.paperless-ngx = {
+    enable = mkEnableOption "paperless-ngx";
+  };
+
+  config = mkIf cfg.enable {
+    services.paperless = {
+      enable = true;
+      address = "127.0.0.1";
+      user = "paperless"; # Default: 'paperless'
+      consumptionDir = "${config.services.paperless.dataDir}/consume";
+      # Allow all users can write to the consumption directory if set to true.
+      consumptionDirIsPublic = false; # Default: false
+      dataDir = "/var/db/paperless";
+      mediaDir = "${config.services.paperless.dataDir}/media";
+      domain = "paperless.example.com"; # Default: null
+      environmentFile = "path/to/environment-file";
+      passwordFile = "/run/keys/paperless-password";
+      port = 28981; # Default: 28981
+      # Configure local PostgreSQL DB server.
+      database.createLocally = true; # Default: false
+      exporter = {
+        enable = true; # Default: false
+        directory = "${config.services.paperless.dataDir}/export";
+        # Schedule when to run the exporter.
+        onCalendar = "01:30:00";
+        settings = {
+          compare-checksums = true;
+          delete = true;
+          no-color = true;
+          no-progress-bar = true;
+        };
+      };
+      # Enable a workaround for document classifier timeouts.
+      # This sets `OMP_NUM_THREADS` to 1.
+      # For the detail, see: https://github.com/NixOS/nixpkgs/issues/240591
+      openMPThreadingWorkaround = true; # Default: true
+    };
+  };
+}
