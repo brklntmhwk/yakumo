8ee60c27f40661c37fd472f2164fb8097be090c5
Ohma Togaki <contact@younagi.dev>
2026-03-01 18:13:35 +0900

    chore: modify inheritance

--- modules/nixos/hardware/cpu/amd/default.nix
+++ modules/nixos/hardware/cpu/amd/default.nix
@@ -11,7 +11,7 @@ let
 in
 {
   config = mkIf (elem "cpu/amd" hardwareMods) {
-    hardware.cpu.amd.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
+    hardware.cpu.amd.updateMicrocode = mkDefault config.hardware.enableRedistributableFirmware;
     boot.kernelModules = [ "kvm-amd" ];
   };
 }
