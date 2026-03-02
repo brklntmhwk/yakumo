ccc11e53c91b64afb81202631b2d9d51a0b4c4b1
Ohma Togaki <contact@younagi.dev>
2026-03-02 13:23:42 +0900

    feat: prepare server hosts (niwatazumi, tamazusa, sazanami)

---
+++ hosts/tamazusa/default.nix
@@ -0,0 +1,67 @@
+{
+  config,
+  lib,
+  ...
+}:
+
+{
+  imports = [
+    ../common
+
+    # hardware configurations are scattered around custom modules and so on.
+    # ./hardware-configuration.nix
+  ];
+
+  yakumo = {
+    system = {
+      role = "server";
+      nix = {
+        enableFlake = true;
+      };
+      networking = {
+        manager = "networkmanager";
+        wifi.enable = true;
+      };
+      # persistence.yosuga = {
+      #   enable = true;
+      #   directories = [
+      #     "/etc/nixos"
+      #     "/etc/NetworkManager/system-connections"
+      #   ];
+      #   files = [
+      #     "/etc/machine-id"
+      #     "/etc/ssh/ssh_host_ed25519_key"
+      #     "/etc/ssh/ssh_host_ed25519_key.pub"
+      #     "/etc/ssh/ssh_host_rsa_key"
+      #     "/etc/ssh/ssh_host_rsa_key.pub"
+      #   ];
+      # };
+    };
+    hardware = {
+      modules = [
+        # "cpu/amd"
+        "ssd"
+      ];
+    };
+  };
+
+  # fileSystems = {
+
+  # };
+
+  i18n = {
+    defaultLocale = "en_US.UTF-8";
+    extraLocaleSettings = {
+      # Run `locale` to see available options.
+    };
+    extraLocales = [
+      "en_US.UTF-8/UTF-8"
+      "en_GB.UTF-8/UTF-8"
+      "es_ES.UTF-8/UTF-8"
+      "ja_JP.UTF-8/UTF-8"
+    ];
+  };
+
+  # Don't modify this unless you're sure about the effects.
+  # system.stateVersion = "25.11";
+}
