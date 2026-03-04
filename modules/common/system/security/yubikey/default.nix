15e8a9d72f5ef723d4942bead27ca74798150c0e
Ohma Togaki <contact@younagi.dev>
2026-03-04 16:20:45 +0900

    chore: tweak yubikey module

--- modules/common/system/security/yubikey/default.nix
+++ modules/common/system/security/yubikey/default.nix
@@ -39,8 +39,14 @@ in
         inherit (pkgs)
           yubikey-manager # The modern CLI tool (ykman)
           yubikey-personalization # The legacy tools
-          yubikey-touch-detector # Lets you know when Yubikey's waiting for skinship.
           ;
+      };
+
+      # Lets you know when Yubikey's waiting for skinship.
+      programs.yubikey-touch-detector = {
+        enable = true;
+        libnotify = true; # Default: true
+        unixSocket = true; # Default: true
       };
     }
     (mkIf isLinux {
