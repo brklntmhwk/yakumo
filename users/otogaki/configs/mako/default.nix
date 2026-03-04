6a9f8050d0bc542d6ab86af6374e39d30e4ef63e
Ohma Togaki <contact@younagi.dev>
2026-03-05 01:35:32 +0900

    chore: tweak mako config

--- users/otogaki/configs/mako/default.nix
+++ users/otogaki/configs/mako/default.nix
@@ -6,6 +6,8 @@ in
 {
   actions = true;
   anchor = "bottom-right";
+  default-timeout = 5000; # Miliseconds.
+  history = true;
   background-color = colors.bg-dim;
   border-color = colors.border;
   border-radius = 0;
