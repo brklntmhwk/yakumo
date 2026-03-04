e13233047f0ae4c1c8bead57ba4d2738e78ac01f
Ohma Togaki <contact@younagi.dev>
2026-03-04 10:07:28 +0900

    chore: tweak service modules

--- modules/nixos/services/home-assistant/default.nix
+++ modules/nixos/services/home-assistant/default.nix
@@ -9,72 +9,102 @@ let
   inherit (lib)
     mkEnableOption
     mkIf
+    mkOption
+    types
     ;
   cfg = config.yakumo.services.home-assistant;
 in
 {
   options.yakumo.services.home-assistant = {
     enable = mkEnableOption "home-assistant";
-  };
-
-  config = mkIf cfg.enable {
-    services.home-assistant = {
-      enable = true;
-      openFirewall = false; # Default: false
-      configDir = "/var/lib/hass";
-      # Allow mutable config editing via HA's web UI if set to true.
-      # This only has an effect if the config option is set.
-      # Note that those mutable changes will be wiped out at every start of the service.
-      configWritable = false; # Default: false
-      config = {
-        homeassistant = {
-          name = "Home";
-          latitude = "!secret latitude";
-          longitude = "!secret longitude";
-          elevation = "!secret elevation";
-          unit_system = "metric";
-          time_zone = "UTC";
-        };
-      };
-      # For the available components, see `pkgs.home-assistant-custom-components`.
-      customComponents = [ ];
-      defaultIntegrations = [
-        "application_credentials"
-        "frontend"
-        "hardware"
-        "logger"
-        "network"
-        "system_health"
-        "automation"
-        "person"
-        "scene"
-        "script"
-        "tag"
-        "zone"
-        "counter"
-        "input_boolean"
-        "input_button"
-        "input_datetime"
-        "input_number"
-        "input_select"
-        "input_text"
-        "schedule"
-        "timer"
-        "backup"
-      ];
-      extraArgs = [ ];
-      # Specify additional packages to add to `propagatedBuildInputs`.
-      extraPackages = [ ];
-      blueprints = {
-        automation = [ ];
-        script = [ ];
-        template = [ ];
-      };
-      # For the available components, see `pkgs.home-assistant-custom-lovelace-modules`.
-      customLovelaceModules = [ ];
-      lovelaceConfig = null; # Default: null
-      lovelaceConfigFile = null; # Default: null
-      lovelaceConfigWritable = false; # Default: false
+    domain = mkOption {
+      type = types.str;
+      default = "localhost";
+      description = "Domain name.";
     };
   };
+
+  config = mkIf cfg.enable (
+    let
+      haCfg = config.services.home-assistant;
+    in
+    {
+      services.home-assistant = {
+        enable = true;
+        openFirewall = false; # Default: false
+        configDir = "/var/lib/hass";
+        # Allow mutable config editing via HA's web UI if set to true.
+        # This only has an effect if the config option is set.
+        # Note that those mutable changes will be wiped out at every start of the service.
+        configWritable = false; # Default: false
+        config = {
+          default_config = { };
+          homeassistant = {
+            name = "Home";
+            external_url = "https://${cfg.domain}";
+            internal_url = "https://${cfg.domain}";
+            latitude = "!secret latitude";
+            longitude = "!secret longitude";
+            elevation = "!secret elevation";
+            unit_system = "metric";
+            time_zone = "UTC";
+            packages.manual = "!include manual.yaml";
+          };
+          http = {
+            server_port = 8123;
+            trusted_proxies = [ ];
+            # use_x_forwarded_for = true;
+          };
+        };
+        # For the available components, see `pkgs.home-assistant-custom-components`.
+        customComponents = [ ];
+        defaultIntegrations = [
+          "application_credentials"
+          "frontend"
+          "hardware"
+          "logger"
+          "network"
+          "system_health"
+          "automation"
+          "person"
+          "scene"
+          "script"
+          "tag"
+          "zone"
+          "counter"
+          "input_boolean"
+          "input_button"
+          "input_datetime"
+          "input_number"
+          "input_select"
+          "input_text"
+          "schedule"
+          "timer"
+          "backup"
+        ];
+        extraArgs = [ ];
+        # Specify additional packages to add to `propagatedBuildInputs`.
+        extraPackages = [ ];
+        blueprints = {
+          automation = [ ];
+          script = [ ];
+          template = [ ];
+        };
+        # For the available components, see `pkgs.home-assistant-custom-lovelace-modules`.
+        customLovelaceModules = [ ];
+        lovelaceConfig = null; # Default: null
+        lovelaceConfigFile = null; # Default: null
+        lovelaceConfigWritable = false; # Default: false
+      };
+
+      services.caddy.virtualHosts = {
+        "${cfg.domain}" = {
+          useACMEHost = "yakumo.net";
+          extraConfig = ''
+            reverse_proxy localhost:${haCfg.config.http.server_port}
+          '';
+        };
+      };
+    }
+  );
 }
--- modules/nixos/services/influxdb/default.nix
+++ modules/nixos/services/influxdb/default.nix
@@ -9,21 +9,72 @@ let
   inherit (lib)
     mkEnableOption
     mkIf
+    mkOption
+    types
     ;
   cfg = config.yakumo.services.influxdb;
 in
 {
   options.yakumo.services.influxdb = {
     enable = mkEnableOption "influxdb";
+    domain = mkOption {
+      type = types.str;
+      default = "localhost";
+      description = "Domain name.";
+    };
   };
 
   config = mkIf cfg.enable {
-    services.influxdb = {
+    services.influxdb2 = {
       enable = true;
-      group = "influxdb"; # Default: 'influxdb'
-      user = "influxdb"; # Default: 'influxdb'
-      dataDir = "/var/db/influxdb";
-      extraConfig = { };
+      provision = {
+        enable = true;
+        initialSetup = {
+          username = "admin"; # Default: 'admin'
+          bucket = "default";
+          organization = "default";
+          passwordFile = config.sops.secrets.xxx.path;
+          tokenFile = config.sops.secrets.xxx.path;
+          # Set how long the bucket retains data (0 means infinite).
+          retention = 0; # Default: 0
+        };
+        organizations = {
+          machines = {
+            description = "";
+            present = true; # Default: true
+            auths = { };
+            buckets.telegraf = {
+              description = "";
+              present = true; # Default: true
+              retention = 0; # Default: 0
+            };
+          };
+        };
+        users = {
+          otogaki = {
+            passwordFile = config.sops.secrets.xxx.path;
+            present = true; # Default: true
+          };
+        };
+      };
+      settings = {
+        reporting-disabled = true;
+        http-bind-address = "0.0.0.0:${toString 8086}";
+      };
     };
+
+    services.caddy.virtualHosts = (
+      let
+        influxCfg = config.services.influxdb2;
+      in
+      {
+        "${cfg.domain}" = {
+          useACMEHost = "yakumo.net";
+          extraConfig = ''
+            reverse_proxy ${influxCfg.settings.http-bind-address}
+          '';
+        };
+      }
+    );
   };
 }
