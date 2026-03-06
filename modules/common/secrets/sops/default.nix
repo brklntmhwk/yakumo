5a9bb34b43f514eb5af9b9eda926c5ba79837349
Ohma Togaki <contact@younagi.dev>
2026-03-06 15:29:55 +0900

    chore: add age-plugin-yubikey to sops env

--- modules/common/secrets/sops/default.nix
+++ modules/common/secrets/sops/default.nix
@@ -15,9 +15,10 @@
   config =
     let
       inherit (builtins) map;
-      inherit (lib) mkIf;
+      inherit (lib) elem mkIf;
       opensshCfg = config.services.openssh;
       xdgCfg = config.yakumo.xdg;
+      hardwareMods = config.yakumo.hardware.modules;
     in
     {
       sops = {
@@ -26,6 +27,10 @@
         # should be set in each host & user configurations.
         defaultSopsFile = flakeRoot + "/secrets/default.yaml";
         age = {
+          # YubiKey doesn't officially support Age, so we use age-plugin-yubikey
+          # and the PIV feature to register it in a YubiKey.
+          # We use it as the admin key to Sops.
+          plugins = mkIf (elem "token/yubikey/piv" hardwareMods) [ pkgs.age-plugin-yubikey ];
           sshKeyPaths =
             if opensshCfg.enable then
               map (k: k.path) opensshCfg.hostKeys
