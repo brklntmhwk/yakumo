41559d0e3265a0992f4a2f4729afd68ac94c5625
Ohma Togaki <contact@younagi.dev>
2026-03-08 01:07:45 +0900

    chore: tweak swayidle config

--- users/otogaki/configs/swayidle/default.nix
+++ users/otogaki/configs/swayidle/default.nix
@@ -23,26 +23,26 @@
   ];
   timeouts = [
     {
-      timeout = 150;
+      timeout = 180; # seconds (3 min)
       command = "brightnessctl -s set 10";
       resumeCommand = "brightnessctl -r";
     }
     {
-      timeout = 150;
+      timeout = 300; # seconds (5 min)
       command = "brightnessctl -sd rgb:kbd_backlight set 0";
       resumeCommand = "brightnessctl -rd rgb:kbd_backlight";
     }
     {
-      timeout = 300;
+      timeout = 480; # seconds (8 min)
       command = "${systemWideBinPath}/pidof hyprlock || ${systemWideBinPath}/hyprlock";
     }
     {
-      timeout = 330;
+      timeout = 600; # seconds (10 min)
       command = "${systemWideBinPath}/niri msg action power-off-monitors";
       resumeCommand = "${systemWideBinPath}/niri msg action power-on-monitors && brightnessctl -r";
     }
     {
-      timeout = 1800;
+      timeout = 1500; # seconds (25 min)
       command = "${systemWideBinPath}/systemctl suspend";
     }
   ];
