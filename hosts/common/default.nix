b9466208cc3e7a785ea4e404b5eb62651d555fff
Ohma Togaki <contact@younagi.dev>
2026-03-21 19:47:26 +0900

    feat: add acme security module

--- hosts/common/default.nix
+++ hosts/common/default.nix
@@ -1,11 +1,18 @@
-{ config, ... }:
+{ config, lib, ... }:
 
+let
+  inherit (lib) mkIf;
+  systemRole = config.yakumo.system.role;
+in
 {
   yakumo = {
     system = {
       # persistence.yosuga = {
       #   enable = true;
       # };
+    };
+    security = {
+      acme.enable = mkIf (systemRole == "server") true;
     };
     services = {
       openssh.enable = true;
---
+++ modules/nixos/security/acme/default.nix
@@ -0,0 +1,84 @@
+{
+  config,
+  lib,
+  rootPath,
+  ...
+}:
+
+let
+  inherit (lib) mkEnableOption mkIf mkMerge;
+  cfg = config.yakumo.security.acme;
+in
+{
+  options.yakumo.security.acme = {
+    enable = mkEnableOption "acme";
+  };
+
+  config = mkIf cfg.enable (
+    let
+      caddyCfg = config.yakumo.services.caddy;
+    in
+    mkMerge [
+      {
+        security.acme = {
+          # Accept the Certificate Authority's (e.g., Let's Encrypt) Terms of Service.
+          # This is necessary to use ACME.
+          acceptTerms = true;
+          defaults = {
+            credentialFiles = {
+              # API token with DNS:Edit permission (since v3.1.0).
+              CF_DNS_API_TOKEN_FILE = config.sops.secrets."acme/cf_dns_token".path;
+              # API token with Zone:Read permission (since v3.1.0).
+              CF_ZONE_API_TOKEN_FILE = config.sops.secrets."acme/cf_zone_token".path;
+            };
+            # For the valid provider list, see:
+            # https://go-acme.github.io/lego/dns/
+            dnsProvider = "cloudflare"; # Default: null
+            # Enable DNS propagation check. It verifies whether changes to our domain's
+            # DNS records—such as updating an IP address, switching hosting providers,
+            # or modifying email settings—have been successfully updated across the
+            # global network of DNS servers.
+            dnsPropagationCheck = true; # Default: true
+            # Set this to our email address for account creation and correspondence
+            # from the Certificate Authority.
+            # Recommended to use the same email for all certs to avoid account creation
+            # limits.
+            email = "contact@younagi.dev"; # Default: null
+            # Specify which group to run the ACME client (LEGO).
+            group = "acme"; # Default: 'acme'
+          };
+          certs = {
+            # TODO: Consider handling the root domain at the project root as TOML or Nix attrset.
+            # This will automatically be the value of the `domain` option.
+            "yakumo.org" = {
+              extraDomainNames = [
+                "*.yakumo.org"
+              ]; # Default: [ ]
+            };
+          };
+        };
+
+        sops.secrets = {
+          "acme/cf_dns_token" = {
+            sopsFile = rootPath + "/secrets/default.yaml";
+            mode = "0440";
+            group = "acme";
+          };
+          "acme/cf_zone_token" = {
+            sopsFile = rootPath + "/secrets/default.yaml";
+            mode = "0440";
+            group = "acme";
+          };
+        };
+      }
+      (mkIf caddyCfg.enable {
+        # Specify systemd services to call `systemctl try-reload-or-restart` on.
+        security.acme.defaults.reloadServices = [ "caddy" ];
+
+        # Add the Caddy service user to the global ACME group so Caddy can read
+        # every ACME certificate.
+        users.groups.acme.members = [ "caddy" ];
+      })
+    ]
+  );
+}
