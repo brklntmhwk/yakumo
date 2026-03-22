f0dea3035888abc047ed9ac014c5a0cda9d51cce
Ohma Togaki <contact@younagi.dev>
2026-03-23 01:04:51 +0900

    chore: add persistent ssh files

--- hosts/common/default.nix
+++ hosts/common/default.nix
@@ -27,6 +27,8 @@ in
         ++ optional (netManager == "networkmanager") "/etc/NetworkManager/system-connections";
         files = [
           "/etc/machine-id"
+          "/etc/ssh/ssh_host_ecdsa_key"
+          "/etc/ssh/ssh_host_ecdsa_key.pub"
           "/etc/ssh/ssh_host_ed25519_key"
           "/etc/ssh/ssh_host_ed25519_key.pub"
           "/etc/ssh/ssh_host_rsa_key"
