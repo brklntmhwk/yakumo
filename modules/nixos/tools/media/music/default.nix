9c65749e3416a6b86ce426716acd24804d4084ff
Ohma Togaki <contact@younagi.dev>
2026-03-25 18:57:01 +0900

    chore: add isLinux guard to nixos media modules

--- modules/nixos/tools/media/music/default.nix
+++ modules/nixos/tools/media/music/default.nix
@@ -14,10 +14,11 @@ let
     types
     ;
   inherit (murakumo.utils) anyHasPrefix;
+  inherit (murakumo.platforms) isLinux;
   mediaMods = config.yakumo.tools.media.modules;
 in
 {
-  config = mkIf (anyHasPrefix "music" mediaMods) {
+  config = mkIf (anyHasPrefix "music" mediaMods && isLinux) {
     yakumo.user.packages = attrValues {
       inherit (pkgs)
         mpv # Classic media player based on MPlayer and mplayer2
--- modules/nixos/tools/media/video/default.nix
+++ modules/nixos/tools/media/video/default.nix
@@ -14,10 +14,11 @@ let
     types
     ;
   inherit (murakumo.utils) anyHasPrefix;
+  inherit (murakumo.platforms) isLinux;
   mediaMods = config.yakumo.tools.media.modules;
 in
 {
-  config = mkIf (anyHasPrefix "video" mediaMods) {
+  config = mkIf (anyHasPrefix "video" mediaMods && isLinux) {
     yakumo.user.packages = attrValues {
       inherit (pkgs)
         mpv # Classic media player based on MPlayer and mplayer2
