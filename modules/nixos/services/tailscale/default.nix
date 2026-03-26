846babada4df741c7284d041d41cb24fe3c5b227
Ohma Togaki <contact@younagi.dev>
2026-03-26 18:16:35 +0900

    refactor: tweak tailscale service module

--- modules/nixos/services/tailscale/default.nix
+++ modules/nixos/services/tailscale/default.nix
@@ -11,9 +11,17 @@ let
     mkEnableOption
     mkIf
     mkMerge
+    mkOption
+types
     ;
   cfg = config.yakumo.services.tailscale;
   meta = config.yakumo.services.metadata.tailscale;
+  routingRoles = [
+    "none"
+    "client"
+    "server"
+    "both"
+  ];
 in
 {
   options.yakumo.services.tailscale = {
@@ -21,12 +29,20 @@ in
     taildrop = {
       enable = mkEnableOption "taildrop feature";
     };
+    routingRole = mkOption {
+      type = types.enum routingRoles;
+      default = "none";
+      description = ''
+        Choice of the routing roles required for Tailscale's routing features, such as
+        subnet routers and exit nodes.
+      '';
+    };
   };
 
   config = mkIf cfg.enable (
     let
       tailCfg = config.services.tailscale;
-      # headMeta = config.yakumo.services.metadata.headscale;
+      headMeta = config.yakumo.services.metadata.headscale;
     in
     mkMerge [
       {
@@ -49,29 +65,73 @@ in
           # `tailscale up --auth-key AUTH_KEY_FILE AUTH_KEY_PARAMS... UP_FLAGS...`
           # https://tailscale.com/docs/reference/tailscale-cli#up
           extraUpFlags = [
-            # "--login-server"
-            # "https://${headMeta.domain}"
+            # Bypass the official Tailscale control plane and talk directly to your server.
+            "--login-server=https://${headMeta.domain}"
           ]; # Default: [ ]
           interfaceName = "tailscale0"; # Default: 'tailscale0'
           # Specify the username or user ID of whom is allowed to fetch
           # Tailscale TLS certificates for the node.
+          # As we use the self-hosted Headscale server, leave this `null` and let
+          # the Headscale server itself have a valid TLS certificate, not the
+          # individual Tailscale clients.
           permitCertUid = null; # Default: null
           # Enable settings required for Tailscale's routing features like
           # subnet routers and exit nodes.
+          # Options:
+          # - 'client': Allows asymmetrical routing so our web traffic can securely
+          # exit from another node.
+          # - 'server': Automatically enables IP forwarding in the kernel.
+          # - 'both': Does of them.
+          # - 'none': Does nothing.
           # If set to 'client' or 'both', reverse path filtering will be set
-          # to loose instead of strict.
-          # If set to 'server' or 'both', IP forwarding will be enabled.
-          useRoutingFeatures = "none"; # Default: 'none' (Options: 'client', 'server', 'both')
+          # to 'loose' instead of 'strict'.
+          # If set to 'server' or 'both', IP forwarding will be enabled
+          # (i.e., `boot.kernel.sysctl."net.{ipv4|ipv6}.conf.all.forwarding" = true`).
+          useRoutingFeatures = cfg.routingRole; # Default: 'none'
         };
 
         networking.firewall = {
           enable = true;
-          trustedInterfaces = [ "tailscale0" ];
+          trustedInterfaces = [ tailCfg.interfaceName ];
           allowedUDPPorts = [ tailCfg.port ];
+          # Perform a reverse path filter test on a packet.
+          # Options: 'strict', 'loose', or bool
+          # - 'strict' (or `true`): Drops the packet if it's asymmetrical: A response
+          # to the packet isn't sent via the same interface as the one the packet
+          # arrives on.
+          # - 'loose': Only drops the packet if the source address is not reachable
+          # via any interfaces.
+          # - `false`: Disables it.
+          # Set it to "loose" to prevent the firewall from aggressively dropping
+          # asymmetrical VPN routing. This is required to use Tailscale Exit Nodes.
+          # Also, setting `services.tailscale.useRoutingFeatures` to either 'client'
+          # or 'both' automatically sets this to 'loose'.
+          # checkReversePath = "loose"; # Default: true
         };
 
         # TODO: Consider introducing nftables.
         # See: https://wiki.nixos.org/wiki/Tailscale#Native_nftables_Support_(Modern_Setup)
+
+        yakumo =
+          let
+            yosugaCfg = config.yakumo.system.persistence.yosuga;
+          in
+          mkMerge [
+            (mkIf yosugaCfg.enable {
+              system.persistence.yosuga = {
+                directories = [
+                  {
+                    path = "/var/lib/tailscale";
+                    mode = "0700";
+                  }
+                  {
+                    path = "/var/cache/tailscale";
+                    mode = "0750";
+                  }
+                ];
+              };
+            })
+          ];
 
         sops.secrets = {
           "tailscale/auth_key_file" = {
