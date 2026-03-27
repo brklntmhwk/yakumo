10e7f1b5737f34fd724e590dc33e10183fe049da
Ohma Togaki <contact@younagi.dev>
2026-03-27 16:29:31 +0900

    feat: add darwin support to xdg module

--- modules/common/desktop/xdg/default.nix
+++ modules/common/desktop/xdg/default.nix
@@ -8,7 +8,7 @@
 
 let
   inherit (lib) mkEnableOption mkIf mkMerge;
-  inherit (murakumo.platforms) isLinux;
+  inherit (murakumo.platforms) isDarwin isLinux;
   cfg = config.yakumo.desktop.xdg;
   userCfg = config.yakumo.user;
 in
@@ -17,53 +17,67 @@ in
     enable = mkEnableOption "XDG Base Directory";
   };
 
-  config = mkIf cfg.enable (mkMerge [
-    {
-      assertions = [
-        {
-          assertion = userCfg ? name;
-          message = "yakumo.desktop.xdg requires yakumo.user.name to be set.";
-        }
+  config = mkIf cfg.enable (
+    let
+      xdgDirs = [
+        "Desktop"
+        "Documents"
+        "Downloads"
+        "Music"
+        "Pictures"
+        "Public"
+        "Templates"
+        "Videos"
+        ".config"
+        ".local/share"
+        ".local/state"
+        ".cache"
       ];
-
-      environment.sessionVariables = {
-        XDG_CACHE_HOME = "$HOME/.cache";
-        XDG_CONFIG_HOME = "$HOME/.config";
-        XDG_DATA_HOME = "$HOME/.local/share";
-        XDG_STATE_HOME = "$HOME/.local/state";
-      };
-
-      # This package provides `xdg-user-dirs-update`, which generates
-      # '~/.config/user-dirs.dirs'. Some apps consult this file to know where
-      # your "Music" or "Downloads" folders actually are.
-      environment.systemPackages = [ pkgs.xdg-user-dirs ];
-    }
-    (mkIf isLinux (
-      let
-        xdgDirs = [
-          "Desktop"
-          "Documents"
-          "Downloads"
-          "Music"
-          "Pictures"
-          "Public"
-          "Templates"
-          "Videos"
-          ".config"
-          ".local/share"
-          ".local/state"
-          ".cache"
+    in
+    mkMerge [
+      {
+        assertions = [
+          {
+            assertion = userCfg ? name && userCfg ? home;
+            message = "`yakumo.desktop.xdg` requires `yakumo.user.name` and `yakumo.user.home` to be set";
+          }
         ];
-        # 'd': Create directory if it does not exist.
+
+        environment.sessionVariables = {
+          XDG_CACHE_HOME = "${userCfg.home}/.cache";
+          XDG_CONFIG_HOME = "${userCfg.home}/.config";
+          XDG_DATA_HOME = "${userCfg.home}/.local/share";
+          XDG_STATE_HOME = "${userCfg.home}/.local/state";
+        };
+      }
+      (mkIf isDarwin {
+        system.activationScripts = {
+          xdgDirsSetup.text =
+            let
+              inherit (lib) concatStringsSep;
+              mkDirCmd = dir: ''
+                mkdir -p "${userCfg.home}/${dir}"
+                chown ${userCfg.name}:staff "${userCfg.home}/${dir}"
+              '';
+            in
+            concatStringsSep "\n" (map mkDirCmd xdgDirs);
+        };
+      })
+      (mkIf isLinux {
+        # This package provides `xdg-user-dirs-update`, which generates
+        # '~/.config/user-dirs.dirs'. Some apps consult this file to know where
+        # your "Music" or "Downloads" folders actually are.
+        environment.systemPackages = [ pkgs.xdg-user-dirs ];
+
+        # 'd': Create directory if it doesn't exist.
         # Otherwise, it safely fixes permissions/ownership without touching content.
         # '%h' for the home directory.
         # Format: Type Path Mode User Group Age Argument
-        mkXdgRule = dir: "d %h/${dir} 0755 - - - -";
-      in
-      {
         # e.g., `[ "d /home/otogaki/Documents 0755 otogaki yakumo - -" ... ]`
-        systemd.user.tmpfiles.rules = map mkXdgRule xdgDirs;
-      }
-    ))
-  ]);
+        systemd.user.tmpfiles.rules = map (
+          dir: "d ${userCfg.home}/${dir} 0755 ${userCfg.name} ${userCfg.group} - -"
+        ) xdgDirs;
+      })
+    ]
+  );
 }
