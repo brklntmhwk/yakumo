890a62b811534ee3d112bb45bd495369c572d2fa
Ohma Togaki <contact@younagi.dev>
2026-04-02 18:21:00 +0900

    chore: tweak garage config

--- modules/nixos/services/garage/default.nix
+++ modules/nixos/services/garage/default.nix
@@ -27,11 +27,40 @@ in
         environmentFile = config.sops.secrets."garage/env_file".path; # Default: null
         extraEnvironment = { };
         logLevel = "info"; # Default: 'info' (Options: 'debug', 'error', 'trace', 'warn')
-        settings = { };
+        settings =
+          let
+            inherit (meta)
+              address
+              bindAddress
+              domain
+              extraPorts
+              port
+              ;
+          in
+          {
+            admin = {
+              api_bind_addr = "${address}:${extraPorts.admin}";
+              admin_token = config.sops.secrets."garage/admin_token".path;
+            };
+            data_dir = "";
+            s3_api = {
+              api_bind_addr = "${address}:${extraPorts.api}";
+              s3_region = "garage";
+              root_domain = domain;
+            };
+            s3_web = {
+              bind_addr = bindAddress;
+              root_domain = domain;
+            };
+            replication_factor = 1;
+          };
       };
 
       sops.secrets = {
         "garage/env_file" = {
+          sopsFile = rootPath + "/secrets/default.yaml";
+        };
+        "garage/admin_token" = {
           sopsFile = rootPath + "/secrets/default.yaml";
         };
       };
